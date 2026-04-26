a323524b98f113917aa27c9479c339af30f04bd8
profxgemini <137665756+profgemini2@users.noreply.github.com>
2026-04-26 10:50:34 +0700

    Create signals.js

---
+++ src/signals.js
@@ -0,0 +1,9 @@
+function generateSignals() {
+  return [
+    { name: "TOKEN_A", volume: 12, activity: 8 },
+    { name: "TOKEN_B", volume: 20, activity: 5 },
+    { name: "TOKEN_C", volume: 7, activity: 15 }
+  ];
+}
+
+module.exports = { generateSignals };
